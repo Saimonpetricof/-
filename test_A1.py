663047120ce146121e42235b9c1d1a19501dfc05
Saimonpetricof <152190347+Saimonpetricof@users.noreply.github.com>
2024-10-18 18:03:03 +0700

    Update test_A1.py

--- test_A1.py
+++ test_A1.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
-def test1(Solution) -> bool:
-		
-		Ans = [1 ,1, 2, 2]
-		Problem = [pd.DataFrame({"id": [1,2,100], "product":[1,1,1]}), 
-						   pd.DataFrame({"id": [1,2,100], "product":[pd.NA,pd.NA,pd.NA]}),
-							 pd.DataFrame({"id": [1,2,100], "product":[pd.NA,1,pd.NA]}),
-							 pd.DataFrame({"id": [1,2,100], "product":[np.nan,1,np.nan]})]
+from A1 import num_distinct
+
+def test1(Solution):	
+		Ans = [3 ,0, 1, 1]
+		Problem = [pd.DataFrame({"id": [1,2,100], "product":[1,2,3]}), 
+						   	pd.DataFrame({"id": [1,2,100], "product":[pd.NA,pd.NA,pd.NA]}),
+							pd.DataFrame({"id": [1,2,100], "product":[pd.NA,1,pd.NA]}),
+							pd.DataFrame({"id": [1,2,100], "product":[np.nan,1,np.nan]})]
 		
 		for index, ans in enumerate(Ans):
 				assert (Solution(Problem[index])) == ans
