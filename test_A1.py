8f8e09a87df016212711dd085a254e14eecd848d
Saimonpetricof <152190347+Saimonpetricof@users.noreply.github.com>
2024-10-18 18:07:56 +0700

    Update test_A1.py

--- test_A1.py
+++ test_A1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from A1 import num_distinct
 
-def test1(Solution):	
+def test1(num_distinct):	
 		Ans = [3 ,0, 1, 1]
 		Problem = [pd.DataFrame({"id": [1,2,100], "product":[1,2,3]}), 
 						   	pd.DataFrame({"id": [1,2,100], "product":[pd.NA,pd.NA,pd.NA]}),
